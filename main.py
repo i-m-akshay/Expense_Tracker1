32575c65ec8e22e069f75e7ad095a37429db40c5
i-m-akshay <akshay.s@moonpreneur.com>
2024-08-20 06:58:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ with tab1:  #Inside the first tab.
     col1, col2 = st.columns([0.3, 0.7])
     with col1:
         st.image("mypic.png", width=200)
-        st.subheader("Ayesha Shariff :sunglasses:")
+        st.subheader("Akshay Sharma :sunglasses:")
 
     with col2:
         st.write(
-            "Hello, I am Akshay, a computer teacher and content developer :computer: . My parents find it challenging to manage and track their expenses. So, I created a website  to help them in tracking and managing their expenses. You can use this website to manage and track your expenses as well. I hope you all like it."
+            "Hello, I am Akshay, a robotics trainer and content developer :robot: . My parents find it challenging to manage and track their expenses. So, I created a website  to help them in tracking and managing their expenses. You can use this website to manage and track your expenses as well. I hope you all like it."
         )
 
 with tab2:  #Inside the second tab.
     st.write(
-        "I love solving math :triangular_ruler: problems and watching ANIME and reading manga sometimes :performing_arts: to learn about different things. Also, i love 3d house plan designing and travelling:sunrise_over_mountains:."
+        "I love coding robots :computer: and playing PC games in my free time :performing_arts:. Also, i love travelling:sunrise_over_mountains:."
     )
 
 with tab3:  #Inside the third tab.
-    st.write("Email : ayeshacse.ai@gmail.com")
-    st.write("Website : https://ayeshacse.home.blog/")
+    st.write("Email : akshay.s@moonpreneur.com")
+    st.write("Website : https://akshay.home.blog/")
